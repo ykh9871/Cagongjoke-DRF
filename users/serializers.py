16234cc15d394c385b37561baaa08c873dc997a8
ykh9871 <ykh9871@gmail.com>
2024-05-31 20:26:07 +0900

    Modify serializers.py to specify the fields to be used among user data

--- users/serializers.py
+++ users/serializers.py
@@ -1,1 +1,26 @@
 from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "username",
+            "email",
+            "password",
+            "last_login",
+            "updated_at",
+        ]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data["username"],
+            email=validated_data["email"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
