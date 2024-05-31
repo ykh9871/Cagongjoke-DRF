2748a707604647cec65e5edb2d71445334e7e52e
ykh9871 <ykh9871@gmail.com>
2024-05-31 20:10:01 +0900

    [Feat] Create serializers.py to be changed in the future

--- cafes/serializers.py
+++ cafes/serializers.py
@@ -1,1 +1,45 @@
 from rest_framework import serializers
+from .models import Region, Cafe, Review, CafeLike, ReviewLike
+from users.serializers import UserSerializer
+
+
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = "__all__"
+
+
+class CafeSerializer(serializers.ModelSerializer):
+    area = RegionSerializer(read_only=True)
+    reviews = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Cafe
+        fields = "__all__"
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    cafe = serializers.StringRelatedField()
+
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class CafeLikeSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    cafe = serializers.StringRelatedField()
+
+    class Meta:
+        model = CafeLike
+        fields = "__all__"
+
+
+class ReviewLikeSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    review = serializers.StringRelatedField()
+
+    class Meta:
+        model = ReviewLike
+        fields = "__all__"
