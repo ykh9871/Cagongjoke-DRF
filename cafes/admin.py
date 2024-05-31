1ede5fb6740a10a3883b3f5f92f5fd3a6fb0b540
ykh9871 <ykh9871@gmail.com>
2024-05-31 20:04:46 +0900

    [Feat] Create admin.py to use the django Admin feature

--- cafes/admin.py
+++ cafes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from cafes.models import (
+    Region,
+    Cafe,
+    Review,
+    CafeLike,
+    ReviewLike,
+)
 
-# Register your models here.
+admin.site.register(Region)
+admin.site.register(Cafe)
+admin.site.register(Review)
+admin.site.register(CafeLike)
+admin.site.register(ReviewLike)
