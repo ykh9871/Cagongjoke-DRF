0fc8752cfeef1a62dce0407c01ffb33e5a764210
woody <woody@woodyui-MacBookAir.local>
2024-06-01 03:18:54 +0900

    [feat] Add is_crawled column

--- apps/cagong/models.py
+++ apps/cagong/models.py
@@ -17,6 +17,7 @@ class Region(models.Model):
 class Cafe(models.Model):
     id = models.IntegerField(primary_key=True)
     crawl_id = models.CharField(null=True, max_length=20)
+    is_crawled = models.BooleanField(default=False)
     name = models.CharField(max_length=100)
     wordcloud_data = models.TextField(null=True, blank=True)
     area = models.ForeignKey(
