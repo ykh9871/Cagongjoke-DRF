f729ced35aca24f6c321d5aaa75d8a27755c9d05
ykh9871 <ykh9871@gmail.com>
2024-06-02 04:08:18 +0900

    [Feat] Add WithdrawalSerializer, ChangePasswordSerializer, UpdateUserSerializer and modify UserSerializer

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -3,6 +3,8 @@ from apps.users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True)
+    password_confirm = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
@@ -11,12 +13,24 @@ class UserSerializer(serializers.ModelSerializer):
             "username",
             "email",
             "password",
+            "password_confirm",
             "last_login",
             "updated_at",
         ]
         extra_kwargs = {"password": {"write_only": True}}
 
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("A user with this email already exists.")
+        return value
+
+    def validate(self, data):
+        if data["password"] != data["password_confirm"]:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
     def create(self, validated_data):
+        validated_data.pop("password_confirm")
         user = User.objects.create(
             username=validated_data["username"],
             email=validated_data["email"],
@@ -24,3 +38,26 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data["password"])
         user.save()
         return user
+
+
+class WithdrawalSerializer(serializers.Serializer):
+    password = serializers.CharField(write_only=True)
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    current_password = serializers.CharField(write_only=True, required=True)
+    new_password = serializers.CharField(write_only=True, required=True)
+
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["username"]
+        extra_kwargs = {"username": {"required": False}}
+
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                "A user with this username already exists."
+            )
+        return value
