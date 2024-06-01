aec91f411242c6d4721e6ff438254c3d2d326123
ykh9871 <ykh9871@gmail.com>
2024-06-02 04:09:34 +0900

    [Feat] Add added logics path

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from .views import (
     SignupAPIView,
     AuthAPIView,
+    WithdrawalViewAPIView,
+    ChangePasswordAPIView,
+    UpdateUserAPIView,
     google_login,
     google_callback,
 )
@@ -14,8 +17,14 @@ urlpatterns = [
     path("signup/", SignupAPIView.as_view(), name="signup"),
     # post - 로그인, get - 회원 정보, delete - 로그아웃
     path("auth/", AuthAPIView.as_view()),
+    # delete - 회원탈퇴
+    path("withdrawal/", WithdrawalViewAPIView.as_view(), name="withdrawal"),
+    # post - 비밀번호 변경
+    path("changePassword/", ChangePasswordAPIView.as_view(), name="changePassword"),
+    # patch - 회원정보 변경
+    path("updateUser/", UpdateUserAPIView.as_view(), name="updateUser"),
     # jwt 토큰 재발급
-    path("auth/refresh/", TokenRefreshView.as_view()),
+    path("auth/refresh/", TokenRefreshView.as_view(), name="reissueToken"),
     # 구글 소셜로그인
     path("google/login/", google_login, name="google_login"),
     path("google/callback/", google_callback, name="google_callback"),
