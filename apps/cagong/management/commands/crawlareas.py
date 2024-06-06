49de60bbafd0ad66741e9b5d35718786cf30372e
woody <woody@woodyui-MacBookAir.local>
2024-06-06 20:49:19 +0900

    [fix] change crawler to use s3 data and Remove sql files

--- apps/cagong/management/commands/crawlareas.py
+++ apps/cagong/management/commands/crawlareas.py
@@ -5,80 +5,24 @@ from apps.cagong.models import Area
 import logging
 
 from datetime import datetime
-import requests
-import time
-import pandas as pd
-from bs4 import BeautifulSoup
+from common.utils import get_dataframe_from_s3
+
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
-url = "https://www.juso.go.kr/info/RoadNameDataList.do?type=search&roadCd=&keyword=&city1=11&county1=&town1=&searchType=0&extend=true"
+bucket = "ca-devbucket"
+file_key = "gisp-data-20240606/area.parquet"
 
 
 class Command(BaseCommand):
     help = "crawl areas"
 
-    def extract(self):
-        logger.info(f"#### Start to extract data")
-        response = requests.get(url)
-        if response.status_code == 200:
-            html = response.text
-        soup = BeautifulSoup(html, "html.parser")
-
-        cities = soup.select("select#city1 option")
-        city_dict = dict()
-        for city in cities:
-            value = city.attrs["value"]
-            if value:
-                title = city.attrs["title"]
-                city_dict[value] = title
-
-        rows = list()
-        for city in city_dict:
-            logger.info(f"start to crawl {city_dict[city]}")
-            response = requests.get(
-                f"https://www.juso.go.kr/getAreaCode.do?from=city&to=county&valFrom={city}&valTo=&rdIndex=undefined"
-            )
-            if response.status_code == 200:
-                xml = response.text
-                soup = BeautifulSoup(xml, features="lxml")
-                values = [value.text for value in soup.find_all("value")]
-                names = [name.text for name in soup.find_all("name")]
-                county_dict = dict(zip(values, names))
-
-                for county in county_dict:
-                    time.sleep(0.3)
-                    response = requests.get(
-                        f"https://www.juso.go.kr/getAreaCode.do?from=county&to=town&valFrom={city}{county}&valTo=&rdIndex=undefined"
-                    )
-                    if response.status_code == 200:
-                        xml = response.text
-                        soup = BeautifulSoup(xml, features="lxml")
-                        values = [value.text for value in soup.find_all("value")]
-                        names = [name.text for name in soup.find_all("name")]
-                        for value, name in zip(values, names):
-                            city_code = city
-                            city_name = city_dict[city]
-                            county_code = county
-                            county_name = county_dict[county]
-                            town_code = value
-                            town_name = name
-                            row = {
-                                "city_code": city_code,
-                                "city_name": city_name,
-                                "county_code": county_code,
-                                "county_name": county_name,
-                                "town_code": town_code,
-                                "town_name": town_name,
-                            }
-                            rows.append(row)
-        return rows
-
-    def process(self, data):
-        logger.info("#### Start to process data")
-        df = pd.DataFrame(data)
+    def extract_process(self):
+        logger.info("#### Start to extract, process data")
+        df = get_dataframe_from_s3(bucket, file_key)
         df["id"] = df["city_code"] + df["county_code"] + df["town_code"]
+        df["county_name"] = df["county_name"].fillna("")
         logger.info("#### Success to process data")
         return df
 
@@ -105,6 +49,5 @@ class Command(BaseCommand):
         logger.info(f"#### Success to load data to Area table")
 
     def handle(self, **options):
-        data = self.extract()
-        df = self.process(data)
+        df = self.extract_process()
         self.load(df)
