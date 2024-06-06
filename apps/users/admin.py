087ce7500335d00adb5ef8989c7fef517e845834
ykh9871 <ykh9871@gmail.com>
2024-06-06 23:25:11 +0900

    [Feat] 장고 admin 페이지에 User 기능 추가
    - 장고 admin 페이지에 User 관련 soft, hard delete restore 기능 추가
    - is_activate가 False인 유저 볼 수 있도록 변경

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,4 +1,52 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from apps.users.models import User
 
-admin.site.register(User)
+
+def soft_delete(modeladmin, request, queryset):
+    queryset.update(is_active=False)
+
+
+soft_delete.short_description = "선택된 user를 Soft Delete 합니다."
+
+
+def hard_delete(modeladmin, request, queryset):
+    queryset.hard_delete()
+
+
+hard_delete.short_description = "선택된 user를 Hard Delete 합니다."
+
+
+def restore(modeladmin, request, queryset):
+    queryset.restore()
+
+
+restore.short_description = "선택된 user를 Restore 합니다."
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "email",
+        "username",
+        "is_active",
+        "is_staff",
+        "created_at",
+        "deleted_at",
+    )
+    list_filter = ("is_active",)  # is_active 필터 추가
+    actions = [soft_delete, hard_delete, restore]  # restore 액션 추가
+
+    def is_active(self, obj):
+        if not obj.is_active:
+            return format_html('<span style="color: red;">{}</span>', obj.is_active)
+        return obj.is_active
+
+    is_active.boolean = True  # True/False 아이콘 표시
+
+    def get_queryset(self, request):
+        # 모든 유저를 반환하도록 queryset을 수정합니다.
+        return User.objects.all_with_deleted()
+
+
+admin.site.register(User, UserAdmin)
